ae99f330b9d6579a1821bf43547a60b14a34ad83
Jean Dalpra <03497876038@senacrs.edu.br>
2024-11-12 17:39:54 -0300

    base

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var header = document.getElementById("meuElemento");
 var atras = document.getElementById("branco")
 const btn2 = document.getElementById('btn2');
-
  btn2.addEventListener('click', () => {
     if(header.className === "header"){
         header.classList.add("header2")
@@ -9,16 +8,12 @@ const btn2 = document.getElementById('btn2');
         btn2.classList.remove("btn2")
         btn2.classList.add("btn2cima")
         atras.classList.remove("headerdefundo");
-        
-        
-     
     }else{
         header.classList.add("header");
         header.classList.remove("header2");
         btn2.classList.remove("btn2cima");
         btn2.classList.add("btn2");
         atras.classList.add("headerdefundo");
-        
     }
  console.log(header.className);
  });
