0729a6e1f345dcf9ce7c1ab58f6d6c2d13944d38
Jean Dalpra <03497876038@senacrs.edu.br>
2025-02-01 16:21:47 -0300

    atualizações

---
+++ ia.js
@@ -0,0 +1,23 @@
+window.botpress.init({
+    "botId": "d8954624-2d75-4bde-9f6c-9c3f1d0b1ee3",
+    "configuration": {
+      "composerPlaceholder": "conversar...",
+      "botName": "AnatoBot",
+      "botAvatar": "https://files.bpcontent.cloud/2025/01/24/23/20250124232554-WLAG97V5.png",
+      "botDescription": "Um Chatbot sobre Anatomia humana",
+      "website": {},
+      "email": {},
+      "phone": {},
+      "termsOfService": {},
+      "privacyPolicy": {},
+      "color": "#c9e3ae",
+      "variant": "solid",
+      "themeMode": "light",
+      "fontFamily": "inter",
+      "radius": 4,
+      "margin-rigth": 100,
+      "additionalStylesheetUrl": "https://files.bpcontent.cloud/2025/01/24/23/20250124232851-LXMAMTP1.css",
+      "allowFileUpload": true
+    },
+    "clientId": "836dfd22-bc9b-4194-9720-ece21f158885"
+  });
